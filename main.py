ab5c264598e2894e236d03dd530e2c566e11440f
diamondrel <49543759+diamondrel@users.noreply.github.com>
2022-02-02 13:04:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
 import yaml
-yamFile = yaml.load(open('/.github/workflows/github-actions-demo.yaml'))
-yamFileTwo = yaml.load(open('/.github/workflows/database.yaml'))
-discord_token_secret = yamFile['jobs']['build']['steps']['env']['token']
-channel_name_secret = yamFile['jobs']['build']['steps']['env']['channel']
-discord_token_better = yamFileTwo['database']['env']['token']
-channel_name_better = yamFileTwo['database']['env']['channel']
+with open('/.github/workflows/database.yaml','r') as file:
+  yamFileOne = yaml.safe_load(file)
+discord_token_secret = yamFileOne['database']['env']['token']
+channel_name_secret = yamFileOne['database']['env']['channel']
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_option
