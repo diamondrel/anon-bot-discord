02582b58dc6046d88861c3bbf06dd1e59c2797f1
diamondrel <49543759+diamondrel@users.noreply.github.com>
2022-02-02 16:55:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
 import os
-discord_token_secret = os.environ['token'] 
-channel_name_secret = os.environ['channel'] 
-from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_option
-client = commands.Bot(command_prefix='!')
-slash = SlashCommand(client, sync_commands=True)
+import discord
 
-@slash.slash(
-  name="anon",
-  description="Sends a Message Anonymously",
-  guild_ids=[691127432165589013],
-  options=[
-    create_option(
-      name="text",
-      description="Enter Text",
-      required=True,
-      option_type=3
-    )
-  ]
-)
+discord_token_secret = os.environ['token']
+channel_name_secret = os.environ['channel']
+bot = discord.Bot()
 
-async def _hello(ctx:SlashContext, text:str):
-  await client.wait_until_ready()
-  channel = client.get_channel(938277429720530954)
+@bot.slash_command(guild_ids=[691127432165589013])
+async def anon(ctx, text):
+  channel = bot.get_channel(938277429720530954)
   await channel.send(text)
-  await ctx.send("Sent!",hidden=True,)
+  await ctx.respond("Sent!", ephemeral=True)
 
-client.run(discord_token_secret)
+bot.run(discord_token_secret)
